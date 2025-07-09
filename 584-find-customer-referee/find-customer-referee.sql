9360012bc1f9ef3dce4087d7bd8011b7d0941b83
ASh <114547296+AlamShihab@users.noreply.github.com>
2025-07-09 22:26:16 +0600

    Time: 426 ms (95.45%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,13 @@
+/*
 SELECT name
 FROM   customer
 WHERE  referee_id <> 2
         OR referee_id IS NULL;
+
+*/
+
+# Method 1:
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+  OR referee_id IS NULL;
