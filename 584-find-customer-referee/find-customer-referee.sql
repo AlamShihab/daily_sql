61d32a655d73812185e078b1b032c174be3d7bbb
ASh <114547296+AlamShihab@users.noreply.github.com>
2025-07-09 22:28:10 +0600

    Time: 511 ms (28.21%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -6,8 +6,20 @@ WHERE  referee_id <> 2
 
 */
 
+/*
 # Method 1:
 SELECT name
 FROM Customer
 WHERE referee_id != 2
   OR referee_id IS NULL;
+
+*/
+
+# Method 2:
+SELECT name
+FROM Customer c1
+WHERE NOT EXISTS (
+  SELECT name
+  FROM Customer c2
+  WHERE referee_id = 2
+    AND c2.id = c1.id);
